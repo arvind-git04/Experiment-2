a82d8e52b218658935646936eaec78455122fae8
Arvind Yadav <arvind7765y@gmail.com>
2026-01-31 21:55:13 +0530

    Add files via upload

---
+++ Experiment-2/Ex-2.2/script.js
@@ -0,0 +1,10 @@
+function filter(category) {
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(card => {
+            if (category === 'all' || card.classList.contains(category)) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    }
